a0d158e4ccb8433ee0bb15dc210ddb9de1710668
PizzaLovingNerd <cameron@stillhq.io>
2025-10-21 18:16:11 -0700

    Added Mutter Patch, fixed GNOME

--- files/scripts/30-packages.sh
+++ files/scripts/30-packages.sh
@@ -12,6 +12,7 @@ dnf install -y --nobest  \
 echo "Swapping GNOME"
 dnf remove -y gnome-shell-extension-background-logo
 dnf swap -y gnome-shell https://download.copr.fedorainfracloud.org/results/still/stillos-alma/epel-10-x86_64/09147899-gnome-shell/gnome-shell-47.4-2.el10.still.2.x86_64.rpm
+dnf swap -y mutter https://download.copr.fedorainfracloud.org/results/still/stillos-alma/epel-10-x86_64/09686403-mutter/mutter-47.5-8.el10.x86_64.rpm
 
 echo "Installing stillOS Packages"
 dnf install -y https://kojipkgs.fedoraproject.org//packages/gnome-shell-extension-just-perfection/34.0/1.el10_1/noarch/gnome-shell-extension-just-perfection-34.0-1.el10_1.noarch.rpm
